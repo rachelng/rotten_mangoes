dcd4e8ae8204d1cd1a831daaba96e4265274eda5
Rachel Ng <racehl@rachel-ng.com>
2014-01-28 01:38:13 +0000

    Created Sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id # auto login
       redirect_to movies_path
     else
       render :new
     end
   end
+
 
   protected
 
