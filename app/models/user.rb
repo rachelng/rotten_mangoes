1a917175e95f78c2159108f655e55712f714955e
Rachel Ng <racehl@rachel-ng.com>
2014-01-28 02:05:05 +0000

    Generated review model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ActiveRecord::Base
 
-  has_secure_password
+	has_many :reviews
 
-  validates :email,
-    presence: true
+	has_secure_password
 
-  validates :firstname,
-    presence: true
+	validates :email,
+	presence: true
 
-  validates :lastname,
-    presence: true
+	validates :firstname,
+	presence: true
 
-  validates :password,
-    length: { in: 6..20 }, on: :create
+	validates :lastname,
+	presence: true
+
+	validates :password,
+	length: { in: 6..20 }, on: :create
 
 end
